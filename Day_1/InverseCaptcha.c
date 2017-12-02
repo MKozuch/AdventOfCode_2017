51df38954afa1449062d2f114b5d2c59ed870b8c
MKozuch <zxdsl@student.agh.edu.pl>
2017-12-02 11:10:40 +0100

    part 2 of day 1 added

--- Day_1/InverseCaptcha.c
+++ Day_1/InverseCaptcha.c
@@ -5,28 +5,21 @@
 #include <string.h>
 
 int solveCaptcha(const char*);
+void testOf_solveCaptcha();
+
+int solveCaptcha2(const char*);
+void testOf_solveCaptcha2();
 
 int main(int argc, char *argv[])
 {
 	 if(argc == 1){
-		printf("Test\n");
-		
-		char *testData[] = {
-			"1122",
-			"1111",
-			"1234",
-			"91212129"
-		};
-		int testResult[] = {3, 4, 0, 9};
-		
-		for(int i=0; i<4; ++i){
-			printf("Input: %s\n ", testData[i]);
-			printf("\t Actual result:   %d\n", solveCaptcha(testData[i]));
-			printf("\t Expected result: %d\n", testResult[i]);
-		}
+		testOf_solveCaptcha(); 
+		printf("\n");
+		testOf_solveCaptcha2();
 	}
 	else if(argc == 2){
-		printf("%d\n", solveCaptcha(argv[1]));
+		printf("Captcha: %d\n", solveCaptcha(argv[1]));
+		printf("Captcha2: %d\n", solveCaptcha2(argv[1]));
 	}
 	return 0;
 }
@@ -40,4 +33,55 @@ int solveCaptcha(const char *captcha){
 			sum += captcha[i] - 48;
 	}
 	return sum;
+}
+
+void testOf_solveCaptcha(){
+	printf("Test solveCaptcha\n");
+	
+	char *testData[] = {
+		"1122",
+		"1111",
+		"1234",
+		"91212129"
+	};
+	int testResult[] = {3, 4, 0, 9};
+	
+	for(int i=0; i<4; ++i){
+		printf("Input: %s\n ", testData[i]);
+		printf("\t Actual result:   %d\n", solveCaptcha(testData[i]));
+		printf("\t Expected result: %d\n", testResult[i]);
+	}
+}
+
+int solveCaptcha2(const char *captcha){
+	int len = strlen(captcha);
+	if(len % 2)
+		return 0;
+	
+	int sum = 0;
+	
+	for(int i=0; i<=len; ++i){
+		if(captcha[i] == captcha[(i+len/2) % len])
+			sum += captcha[i] - 48;
+	}
+	return sum;
+}
+
+void testOf_solveCaptcha2(){
+	printf("Test solveCaptcha2\n");
+	
+	char *testData[] = {
+		"1212",
+		"1221",
+		"123425",
+		"123123",
+		"12131415"
+	};
+	int testResult[] = {6, 0, 4, 12, 4};
+	
+	for(int i=0; i<5; ++i){
+		printf("Input: %s\n ", testData[i]);
+		printf("\t Actual result:   %d\n", solveCaptcha2(testData[i]));
+		printf("\t Expected result: %d\n", testResult[i]);
+	}
 }
